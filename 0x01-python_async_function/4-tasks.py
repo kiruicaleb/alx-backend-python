6f094619b344a93dd2929fd319b9660a3055b3af
kiruicaleb <keringcaleb3@gmail.com>
2023-07-10 08:59:59 +0000

    async function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task 4"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
